2a6611b0785f2376dd7062417b706cc3264db1db
yunji <dbswl5920@gmail.com>
2020-11-13 03:32:40 +0900

    ex05 upload

---
+++ ex05/exexvls.c
@@ -0,0 +1,12 @@
+/* exexvls.c */
+/* execv 로 ls 실행 */
+#include <unistd.h>
+int main()
+{
+char * const argv[] = {"ls", "-l", (char *) 0};
+printf("executing execv.\n");
+execv("/bin/ls", argv);
+/* 만약 execv 이 복귀하면, 호출은 실패. */
+perror("execv failed to run ls");
+exit(1);
+}
